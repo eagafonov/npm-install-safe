14f0d479177296692efa1c5e30ef94b79f67ecab
Eugene Agafonov <e.a.agafonov@gmail.com>
2026-04-15 00:07:57 -0700

    Add -x for command tracing

--- npm-install-safe.sh
+++ npm-install-safe.sh
@@ -6,4 +6,6 @@ QUARANTINE_DAYS="${QUARANTINE_DAYS:-7}"
 BEFORE_DATE="$(date -d "${QUARANTINE_DAYS} days ago" --iso-8601)"
 
 echo "🔒 Safe install: only allowing packages published before ${BEFORE_DATE} (${QUARANTINE_DAYS}-day quarantine)"
+
+set +x
 npm install --before="${BEFORE_DATE}" "$@"
